762382ff30e4c33691f4c99e3cfab621d9193a83
etanguay <etienne.tanguay@cll.qc.ca>
2013-09-06 13:15:39 -0400

    Header avec mes 2 pages

---
+++ header.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+            <title>Lab3 Etienne Tanguay </title>
+    </head>
+        <body>
+            <h6>Le site de Etienne Tanguay ========> </h6>
+            <a href="login.php">Login</a>
+            <a href="secret.php">Page secrete</a>
+        </body>
+</html>
